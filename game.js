649f3f86ec3d7ee998a724cb5e98da55dcaf0d5e
sneha <snehamittal88293@gmail.com>
2022-10-07 17:15:06 +0530

    javascript file

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ var userClickedPattern = [];
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
 })
 
 function nextSequence(){
@@ -14,4 +16,12 @@ function nextSequence(){
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+     audio.play();
+
 }
